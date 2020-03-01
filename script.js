781fb4ee48794e3c6e6247b2b417a5d7ee202e9f
robin <rogo009git@gmail.com>
2020-02-29 19:45:56 -0800

    update script and index files

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modal = document.getElementById("simpleModal");
 // get open modal button
 const modalBtn = document.getElementById("modalBtn");
 
-// get close button, getElements by class name returns an array even if there's only one item. meaning you have to add bracket notation to indicate you want the first selection
+// get close button, getElements by class name returns an array even if there's only one item. meaning you have to add bracket notation [0] to indicate you want the first item
 const closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 // listen for open click
@@ -26,7 +26,7 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-// function to exit on window click; make sure event target is: window
+// function to exit on window click; make sure event target is: modal (the dark area around?)
 function outsideClick(e) {
     if(e.target == modal) {
         modal.style.display = "none";
